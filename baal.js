e5f6c4dd09425a2c32b3ceb9c5c9448e0d313910
Grimmbol <krogstie.oystein@gmail.com>
2020-04-06 16:29:38 +0200

    restructure the module, add listeners and handles for input elements

--- baal.js
+++ baal.js
@@ -1,16 +1,85 @@
 'use strict';
-// TODO: use modules to scope the module. This block hack is icky
+// Prefixing the public methods to avoid polluting the namespace
+let baal_rerender;
+
+// TODO: consider using a class for this, or a ES6 module.
+// The closure based approach is a little weird
 {
-    const frameDelay = 500;
     const canvasContextLineWidth = 3;
-
     const campfireCenter = {x: 250, y: 250};
-    
-    let sliderElement = undefined;
-    let numberOfParticipants = 1;
 
-    // Setup on windo load
+    // Internal state
+    let sliderElement = undefined;
+    let socialDistance = 1; //meters
+    let stickRange = 2; //meters
+    let pointList = []
+
+    // Result values
+    let numParticipants = NaN;
+    let resultDistance = NaN;
+
+    // Handles
+    let canvasContext = undefined;
+    let stickRangeElementHandle = undefined;
+    let socialDistanceElementHandle = undefined;
+    let errorReporterElementHandle = undefined;
+
+    // Public API initialisation
+    baal_rerender = () => {
+	render();
+    }
+    
+    // Setup on window load
     window.onload = (e) => {
+	initialSetup();
+	updateState();
+	render();
+    };
+
+    //*** Setup methods ***
+    function initialSetup() {
+	setupCanvas();
+	setupInputElements();
+	setupOutputElements();
+    }
+
+    function setupInputElements(){
+	setupInputElementHandles();
+	initInputElements();
+    }
+
+    function setupOutputElements() {
+	setupOutputElementHandles();
+    }
+    
+    function initInputElements() {
+	stickRangeElementHandle.onclick = stickRangeUpdateHandler;
+	socialDistanceElementHandle.onclick = socialDistanceUpdateHandler;
+
+	stickRangeElementHandle.value = 2;
+	socialDistanceElementHandle.value = 1;
+    }
+    
+    function setupInputElementHandles() {
+	stickRangeElementHandle = document.querySelector("#range_selector");
+	if(stickRangeElementHandle == undefined) {
+	    console.log("Stick range element not found");
+	    displayError("Stick range element not found");
+	}
+
+	socialDistanceElementHandle = document.querySelector("#social_distance_selector");
+	if(socialDistanceElementHandle == undefined) {
+	    console.log("Social distance element not found");
+	    displayError("Social distance element not found");
+	}
+    }
+
+        
+    function setupOutputElementHandles() {
+	
+    }
+
+    function setupCanvas() {
 	let canvasElement = document.querySelector("#main_canvas");
 	if(canvasElement == undefined) {
 	    console.log("Canvas element not found");
@@ -18,26 +87,57 @@
 	    return;
 	}
 	
-	let canvasContext = canvasElement.getContext("2d");
+	canvasContext = canvasElement.getContext("2d");
 	canvasContext.lineWidth = canvasContextLineWidth;
-	renderMainCanvas(canvasContext);
-    };
-    
-    function renderMainCanvas(canvasContext) {
-	console.log(canvasContext);
-	const pointList = [
-	    {x:  5, y:  5},
-	    {x:  0, y: 10},
-	    {x: 10, y: 10},
-
-	    {x: 50, y:  50},
-	    {x:  0, y: 100},
-	    {x: 10, y:  20}
-	];
-
-	drawTriangles(pointList, canvasContext);
     }
 
+    //*** Event handlers ***
+    function stickRangeUpdateHandler(e) {
+	stickRange = stickRangeElementHandle.valueAsNumber;
+	updateState();
+	render();
+    }
+
+    function socialDistanceUpdateHandler(e) {
+	socialDistance = socialDistanceElementHandle.valueAsNumber;
+	updateState();
+	render();
+    }
+
+    //*** State update***
+    function updateState(){
+	generatePoints();
+	updateOutputValues();
+    }
+
+    function generatePoints() {
+	console.log(
+	    `Distance is ${stickRange} meters, and sticks are ${socialDistance} meters`
+	);
+    }
+
+    function updateOutputValues(newResultDistance, newNumParicipants) {
+	
+    }
+    
+    //*** Rendering methods ***
+    function render() {
+	renderOutputDisplays();
+	renderMainCanvas();
+    }
+    
+    function renderMainCanvas() {
+	
+    }
+
+    function renderOutputDisplays() {
+
+    }
+    
+    function drawRegularPolygon() {
+	
+    }
+    
     //Draws a triangle for every consecutive triplet of points in pointList
     //Excess ponts are discarded
     function drawTriangles(pointList, canvasContext) {
@@ -55,9 +155,11 @@
 	    canvasContext.stroke();
 	}
     }
-    
+
+    // If this is going to be usefull, it needs some way of displaying or
+    // prioritising error messages
     function displayError(errorMessage) {
-	let errorReporterElement = document.querySelector("#error_reporter");
-	errorReporterElement.innerHTML = "Error: " + errorMessage;
+	errorReporterElementHandle = document.querySelector("#error_reporter");
+	errorReporterElementHandle.innerHTML = "Error: " + errorMessage;
     }
 }
